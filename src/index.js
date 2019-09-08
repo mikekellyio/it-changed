536a07ce9951e99a3a2d73f929b9fef80215a48c
Mike Kelly <mike@kellyfam.net>
2019-09-07 21:46:05 -0400

    sketch api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// Write your package code here.
+const itChanged = opts => {
+  const defaults = {
+    /*
+     * called when `detector.add(obj)` is called. detects if object already exists and emits `detector.on('change')
+     */
+    add: obj => {},
+    exists: obj => false,
+    objExists: () => {}
+  };
+  const { add, exists, objExists } = Object.assign(defaults, opts);
+
+  return {
+    add: obj => {
+      let oldObj;
+      if ((oldObj = exists(obj))) {
+        return objExists(oldObj);
+      } else return add(obj);
+    }
+  };
+};
+
+module.exports = itChanged;
--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,45 @@
-test('it puts the lotion in the basket', () => {
-  const basket = ['lotion'];
-  expect(basket.indexOf('lotion')).toBe(0);
-});
+import { JestEnvironment } from "@jest/environment";
+import itChanged from "./index";
 
-test('it throws an error', () => {
-  expect(() => {
-    throw new Error('Something went wrong.');
-  }).toThrow();
+describe("it-changed", () => {
+  it("initializes", () => {
+    let detector = itChanged();
+    expect(detector).toBeDefined();
+    expect(detector.add).toBeDefined();
+  });
+
+  describe("intializes with options", () => {
+    it("sets an add callback", () => {
+      const addCallBack = obj => 42;
+      let detector = itChanged({ add: addCallBack });
+      expect(detector.add()).toBe(42);
+    });
+  });
+
+  describe(".add()", () => {
+    it("calls the add callback", () => {
+      let addCallBack = jest.fn(obj => obj);
+      let detector = itChanged({
+        add: addCallBack
+      });
+
+      let addedObj = { id: "1234", stuff: "testing stuff" };
+      detector.add(addedObj);
+      expect(addCallBack.mock.calls.length).toBe(1);
+    });
+
+    it("doesn't call the add callback when exists === true", () => {
+      let addCallBack = jest.fn(obj => obj);
+      let existsCallback = jest.fn(obj => true);
+      let detector = itChanged({
+        add: addCallBack,
+        exists: existsCallback
+      });
+
+      let addedObj = { id: "1234", stuff: "testing stuff" };
+      detector.add(addedObj);
+      expect(addCallBack.mock.calls.length).toBe(0);
+      expect(existsCallback.mock.calls.length).toBe(1);
+    });
+  });
 });
